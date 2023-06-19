b418803bdf45816fa90250c52e77340efc5f33cb
VlSelikh <vladislav@noona.is>
2023-06-20 00:39:22 +0300

    feat: implement streams and hash tasks

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,13 @@
+import { createReadStream } from "node:fs";
+import { fileURLToPath } from "node:url";
+
 const read = async () => {
-    // Write your code here 
+  const fileName = fileURLToPath(
+    new URL("./files/fileToRead.txt", import.meta.url)
+  );
+  createReadStream(fileName, { encoding: "utf8" }).on("data", (chunk) => {
+    process.stdout.write(chunk);
+  });
 };
 
 await read();
--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,18 @@
+import { Transform, pipeline } from "node:stream";
+
 const transform = async () => {
-    // Write your code here 
+  const { stdin, stdout } = process;
+  const transformStream = new Transform({
+    transform(chunk, encoding, callback) {
+      const reversedData = chunk.toString().split("").reverse().join("");
+      this.push(reversedData);
+      callback();
+    },
+  });
+
+  pipeline(stdin, transformStream, stdout, (err) => {
+    if (err) throw new Error(err.message);
+  });
 };
 
 await transform();
